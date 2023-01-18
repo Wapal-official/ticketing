f70c4b4f9c8d93f9c5c9d79d27be7f0db1944488
Meghant00 <meghant@reconwithme.com>
2023-01-18 16:03:09 +0545

    chore: change design of faq card

--- components/Landing/Faq/FaqCard.vue
+++ components/Landing/Faq/FaqCard.vue
@@ -1,10 +1,38 @@
 <template>
-  <div
-    class="flex flex-col items-center justify-start gap-6 rounded px-8 py-8 faq-card text-white"
-  >
-    <h4 class="text-base"><slot name="question"></slot></h4>
-    <p>
-      <slot name="answer"></slot>
-    </p>
-  </div>
+  <button @click="toggleAnswer">
+    <div
+      class="flex flex-col items-start justify-start rounded px-4 py-4 border border-[#EA59BE] text-white transition-all duration-200 ease-linear"
+      :class="{ 'gap-2 border-none': showAnswer }"
+    >
+      <h4
+        class="text-sm md:text-lg flex flex-row items-center justify-between w-full text-left gap-4 transition-all duration-200 ease-linear"
+        :class="{ 'text-[#EA59BE]': showAnswer }"
+      >
+        <slot name="question"></slot>
+        <v-icon
+          class="!transform"
+          :class="{ '!rotate-90 !text-[#EA59BE]': showAnswer }"
+          >mdi-chevron-right</v-icon
+        >
+      </h4>
+      <p
+        class="overflow-hidden text-left text-sm md:text-base !my-0"
+        :class="{ 'h-full': showAnswer, 'h-0': !showAnswer }"
+      >
+        <slot name="answer"></slot>
+      </p>
+    </div>
+  </button>
 </template>
+<script lang="ts">
+export default {
+  data() {
+    return { showAnswer: false };
+  },
+  methods: {
+    toggleAnswer() {
+      this.showAnswer = !this.showAnswer;
+    },
+  },
+};
+</script>
