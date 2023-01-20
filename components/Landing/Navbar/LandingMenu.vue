beb499d23ea4707090aae144450d4bc65ff0bdaa
Meghant00 <meghant@reconwithme.com>
2023-01-20 12:08:00 +0545

    chore: change size of faq card

--- components/Landing/Navbar/LandingMenu.vue
+++ components/Landing/Navbar/LandingMenu.vue
@@ -2,7 +2,7 @@
   <div
     class="flex flex-col items-start justify-start w-full text-base gap-8 md:w-1/2 lg:w-full lg:flex-row lg:justify-between lg:items-center"
   >
-    <menu-item text="FAQS" link="/faqs" @close="close" />
+    <menu-item text="FAQS" link="/faq" @close="close" />
     <connect-wallet @close="close" />
   </div>
 </template>
