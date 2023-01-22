017ed778d132d2255ab5678d8552c288be4191f7
Meghant00 <meghant@reconwithme.com>
2023-01-22 16:20:19 +0545

    chore: add slider in landing page

---
+++ components/Landing/LandingSlider.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="px-4 py-16">
+    <div class="swiper mySwiper" ref="swiper">
+      <div class="swiper-wrapper">
+        <div
+          class="swiper-slide"
+          v-for="(image, index) in sliderImages"
+          :key="index"
+        >
+          <img
+            :src="image"
+            class="object-fill w-full h-[250px] md:w-[250px] md:h-[250px]"
+            alt="Image"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import Swiper from "swiper/swiper-bundle.min";
+import "swiper/swiper-bundle.min.css";
+import fox from "@/assets/img/fox.png";
+import firstImage from "@/assets/img/800.png";
+import secondImage from "@/assets/img/921.png";
+import thirdImage from "@/assets/img/6896.png";
+export default {
+  data() {
+    return {
+      sliderImages: [],
+      fox,
+      firstImage,
+      secondImage,
+      thirdImage,
+    };
+  },
+  async mounted() {
+    this.sliderImages.push(fox);
+    this.sliderImages.push(firstImage);
+    this.sliderImages.push(secondImage);
+    this.sliderImages.push(thirdImage);
+    this.sliderImages.push(fox);
+    this.sliderImages.push(firstImage);
+    this.sliderImages.push(secondImage);
+    this.sliderImages.push(thirdImage);
+    await this.$nextTick();
+    new Swiper(this.$refs.swiper, {
+      spaceBetween: 16,
+      autoplay: true,
+      loop: true,
+      breakpoints: {
+        375: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+        },
+        768: {
+          slidesPerView: 3,
+          slidesPerGroup: 1,
+        },
+        1024: {
+          slidesPerView: 5,
+          slidesPerGroup: 1,
+        },
+        1920: {
+          slidesPerView: 7,
+          slidesPerGroup: 1,
+        },
+      },
+    });
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,39 @@
 <template>
-  <div
-    class="flex flex-row items-center justify-center gap-8 h-full py-60 md:py-80 lg:py-40"
-  >
-    <div
-      class="text-white px-8 lg:w-[60%] flex flex-col items-center justify-center gap-10 text-center"
-    >
-      <h1
-        class="text-3xl font-semibold text-wapal-gray flex flex-col items-center justify-center gap-2"
+  <div>
+    <section class="flex flex-row items-center justify-center gap-8 py-60">
+      <div
+        class="text-white px-8 lg:w-[60%] flex flex-col items-center justify-center gap-10 text-center"
       >
-        Zero Code, Zero Fee NFT Creator Studio on Aptos
-      </h1>
-      <primary-button @click.native="checkWalletStatus"
-        >Sign Up Early</primary-button
+        <h1
+          class="text-3xl font-semibold text-wapal-gray flex flex-col items-center justify-center gap-2"
+        >
+          Zero Code, Zero Fee NFT Creator Studio on Aptos
+        </h1>
+        <primary-button @click.native="checkWalletStatus"
+          >Sign Up Early</primary-button
+        >
+      </div>
+      <v-dialog
+        v-model="showConnectWalletModal"
+        content-class="!w-full md:!w-1/2 lg:!w-[30%]"
       >
-    </div>
-    <v-dialog
-      v-model="showConnectWalletModal"
-      content-class="!w-full md:!w-1/2 lg:!w-[30%]"
-    >
-      <connect-wallet-modal
-        message="Please Connect your wallet to Sign Up"
-        @closeModal="showConnectWalletModal = false"
-        @walletConnected="displayWalletConnectedMessage"
-      />
-    </v-dialog>
-    <v-dialog
-      v-model="showSignupDialog"
-      content-class="!w-full md:!w-1/2 lg:!w-[35%]"
-    >
-      <signup-modal
-        @close="showSignupDialog = false"
-        @walletConnected="displayWalletConnectedMessage"
-      />
-    </v-dialog>
+        <connect-wallet-modal
+          message="Please Connect your wallet to Sign Up"
+          @closeModal="showConnectWalletModal = false"
+          @walletConnected="displayWalletConnectedMessage"
+        />
+      </v-dialog>
+      <v-dialog
+        v-model="showSignupDialog"
+        content-class="!w-full md:!w-1/2 lg:!w-[35%]"
+      >
+        <signup-modal
+          @close="showSignupDialog = false"
+          @walletConnected="displayWalletConnectedMessage"
+        />
+      </v-dialog>
+    </section>
+    <landing-slider />
   </div>
 </template>
 
@@ -40,9 +41,10 @@
 import PrimaryButton from "@/components/Button/PrimaryButton.vue";
 import ConnectWalletModal from "@/components/ConnectWallet/ConnectWalletModal.vue";
 import SignupModal from "@/components/Signup/SignupModal.vue";
+import LandingSlider from "~/components/Landing/LandingSlider.vue";
 export default {
   name: "IndexPage",
-  components: { PrimaryButton, ConnectWalletModal, SignupModal },
+  components: { PrimaryButton, ConnectWalletModal, SignupModal, LandingSlider },
   data() {
     return {
       showConnectWalletModal: false,
