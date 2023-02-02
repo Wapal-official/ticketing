59e12eeb6e1ba30ebc49d0ffd85ce6655a3b4f81
Meghant00 <meghant@reconwithme.com>
2023-02-02 13:10:46 +0545

    chore: create explore page

--- components/Landing/Navbar/LandingMenu.vue
+++ components/Landing/Navbar/LandingMenu.vue
@@ -7,7 +7,7 @@
         <template v-slot:activator="{ on, attrs }">
           <div v-bind="attrs" v-on="on">
             <NuxtLink
-              to="/faq"
+              to="/explore"
               class="!tw-text-white tw-transition tw-duration-200 tw-ease-linear hover:!tw-text-wapal-pink tw-uppercase !tw-font-normal"
             >
               Explore
