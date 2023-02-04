ba1500e13aaf6e875216fa5d152749c8b8049cfd
Meghant00 <meghant@reconwithme.com>
2023-02-04 10:17:29 +0545

    chore: remove duplicate description

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,6 @@ export default {
       },
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" },
       {
         name: "keywords",
