bcaff5d9e7d8531c2d4aebe2aedaefb4f6479743
Meghant00 <meghant@reconwithme.com>
2023-03-09 16:04:13 +0545

    chore: create axios interceptor

--- services/CollectionService.ts
+++ services/CollectionService.ts
@@ -1,21 +1,18 @@
 import axios from "axios";
 
+import { publicRequest } from "./fetcher";
+
 export const getCollections = async () => {
   const res = await axios.get(`${process.env.baseURL}/api/collection/all`);
   return res.data.data;
 };
 
 export const getCollection = async (collectionId: string) => {
-  const res = await axios.get(
-    `${process.env.baseURL}/api/collection/${collectionId}`
-  );
+  const res = await publicRequest.get(`/api/collection/${collectionId}`);
   return res.data;
 };
 
 export const createCollection = async (collection: any) => {
-  const res = await axios.post(
-    `${process.env.baseURL}/api/collection/create/`,
-    collection
-  );
+  const res = await publicRequest.post(`/api/collection/create/`, collection);
   return res;
 };
