cd3c41773194b8d316cfc90bff0925cd17ff8cbd
Meghant00 <meghant@reconwithme.com>
2023-03-23 17:11:59 +0545

    chore: added api for verification

--- services/VerificationService.ts
+++ services/VerificationService.ts
@@ -5,3 +5,12 @@ export const submitVerificationCode = async (body: any) => {
 
   return res;
 };
+
+export const requestVerification = async (verificationDetails: any) => {
+  const res = await axios.post(
+    `${process.env.baseURL}/api/verification/create`,
+    verificationDetails
+  );
+
+  return res;
+};
