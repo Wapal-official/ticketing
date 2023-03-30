59d83b12cddf9961a62a54b6393d1ad786221b9d
Meghant00 <meghant@reconwithme.com>
2023-03-30 18:31:58 +0545

    chore: fixed link validation in create collection

--- pages/dashboard/create-collection.vue
+++ pages/dashboard/create-collection.vue
@@ -419,10 +419,9 @@ extend("required", {
 
 extend("link", {
   validate(value) {
-    const pattern =
-      /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
-
-    if (!pattern.test(value)) {
+    try {
+      const givenURL = new URL(value);
+    } catch (error) {
       return false;
     }
     return true;
