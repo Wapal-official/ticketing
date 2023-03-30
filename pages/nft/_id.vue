9e636f9bd6940fc5fdf6339a719cc7e05842c92a
Meghant00 <meghant@reconwithme.com>
2023-03-30 21:02:24 +0545

    chore: change twitter meta

--- pages/nft/_id.vue
+++ pages/nft/_id.vue
@@ -250,25 +250,31 @@ export default {
     return {
       title: this.getTitle,
       meta: [
+        { hid: "twitter:title", name: "twitter:title", content: this.getTitle },
+        {
+          hid: "twitter:card",
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        { hid: "twitter:title", name: "twitter:title", content: this.getTitle },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: this.collection.image,
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content: this.collection.description,
+        },
         { hid: "og-type", property: "og:type", content: "website" },
         {
           hid: "og-image",
           property: "og:image",
           content: this.collection.image,
         },
-        { hid: "og-type", property: "og:title", content: this.getTitle },
-        { hid: "t-type", name: "twitter:card", content: "summary_large_image" },
-        { hid: "t-type", name: "twitter:title", content: this.getTitle },
-        {
-          hid: "t-type",
-          name: "twitter:image",
-          content: this.collection.image,
-        },
-        {
-          hid: "t-type",
-          name: "twitter:description",
-          content: this.collection.description,
-        },
+        { hid: "og:title", property: "og:title", content: this.getTitle },
+
         {
           hid: "description",
           name: "description",
