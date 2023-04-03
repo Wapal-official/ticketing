6f6a4e64394624c808023e4199c166cc532972b1
Meghant00 <meghant@reconwithme.com>
2023-04-03 22:25:46 +0545

    chore: change featured collection

--- components/Landing/Banner.vue
+++ components/Landing/Banner.vue
@@ -424,7 +424,7 @@ export default {
     try {
       let res = null;
       if (!process.env.baseURL?.includes("staging")) {
-        res = await getCollection("642af1fa652ca98f05f346d3");
+        res = await getCollection("642afd65652ca98f05f349e5");
       } else {
         res = await getCollection("6411e5928d694e608061b029");
       }
