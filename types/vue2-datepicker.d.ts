6c2d7c5012c8e50a1602c97885d10dcb91805114
Meghant00 <meghant@reconwithme.com>
2023-04-09 17:37:31 +0545

    chore: declare vue2-datepicker module

--- types/vue2-datepicker.d.ts
+++ types/vue2-datepicker.d.ts
@@ -1,1 +1,7 @@
-declare module "vue2-datepicker";
+declare module "vue2-datepicker" {
+  import { VueConstructor } from "vue";
+
+  const DatePicker: VueConstructor;
+
+  export default DatePicker;
+}
