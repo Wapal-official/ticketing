970dbe1526a45a1d98d7384c5ea7c8001c333f8d
Meghant00 <meghant@reconwithme.com>
2023-04-11 13:55:05 +0545

    chore: add social preview in whitelist page

--- pages/whitelist/_id/_name.vue
+++ pages/whitelist/_id/_name.vue
@@ -405,6 +405,43 @@ export default {
     SignupModal,
     ButtonWithLoader,
   },
+  head() {
+    return {
+      title: this.getTitle,
+      meta: [
+        { hid: "twitter:title", name: "twitter:title", content: this.getTitle },
+        {
+          hid: "twitter:card",
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        { hid: "twitter:title", name: "twitter:title", content: this.getTitle },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: this.collection.image,
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content: this.collection.description,
+        },
+        { hid: "og-type", property: "og:type", content: "website" },
+        {
+          hid: "og-image",
+          property: "og:image",
+          content: this.collection.image,
+        },
+        { hid: "og:title", property: "og:title", content: this.getTitle },
+
+        {
+          hid: "description",
+          name: "description",
+          content: this.collection.description,
+        },
+      ],
+    };
+  },
   data() {
     return {
       loading: true,
@@ -800,6 +837,18 @@ export default {
         return true;
       }
       return false;
+    },
+    getTitle() {
+      return this.collection.name
+        ? "Whitelist - " + this.collection.name
+        : "Title";
+    },
+
+    getImage() {
+      return this.collection.image ? this.collection.image : "";
+    },
+    getDescription() {
+      return this.collection.description ? this.collection.description : "";
     },
   },
   async mounted() {
