83d3f9802865b116de227cc2778ff542ce55252b
Pashupati Sah <pashupatisah112@gmail.com>
2023-04-23 11:23:05 +0545

    minor update

--- components/Landing/auctions.vue
+++ components/Landing/auctions.vue
@@ -2,7 +2,7 @@
   <div>
     <v-container fluid>
       <v-row>
-        <v-col v-for="(item, i) in auctions" :key="i" cols="12" lg="3" md="6" align="center">
+        <v-col v-for="(item, i) in auctions" :key="i" cols="12" lg="3" md="6">
           <AuctionCard :auction="item" />
         </v-col>
       </v-row>
