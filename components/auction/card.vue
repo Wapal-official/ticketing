ba1ed03e6cc3e2e163d5b0f96a150e10dc224b97
Meghant00 <meghant@reconwithme.com>
2023-05-24 12:31:16 +0545

    chore: hide place bid button if auction is not started

--- components/auction/card.vue
+++ components/auction/card.vue
@@ -1,60 +1,62 @@
 <template>
-  <div>
+  <NuxtLink
+    :to="`/auctions/${auction.auction_name}`"
+    class="tw-border tw-border-wapal-pink tw-rounded tw-flex tw-flex-col tw-items-start tw-justify-start tw-gap-4 tw-h-full tw-w-full tw-group lg:tw-w-[320px] 3xl:tw-w-[400px]"
+  >
     <div
-      class="tw-border tw-border-wapal-pink tw-rounded tw-flex tw-flex-col tw-items-start tw-justify-start tw-gap-4 tw-h-full tw-w-full lg:tw-w-[320px] 3xl:tw-w-[400px]"
+      class="tw-overflow-hidden tw-w-full tw-min-h-[260px] tw-max-h-[260px] 3xl:tw-min-h-[450px] 3xl:tw-max-h-[450px]"
     >
       <img
         :src="auction.nft.meta.image"
         :alt="auction.nft.meta.name"
-        class="tw-rounded-t tw-w-full tw-min-h-[260px] tw-max-h-[260px] 3xl:tw-min-h-[450px] 3xl:tw-max-h-[450px] tw-object-cover"
+        class="tw-rounded-t tw-w-full tw-min-h-[260px] tw-max-h-[260px] 3xl:tw-min-h-[450px] 3xl:tw-max-h-[450px] tw-object-cover tw-group tw-transform tw-transition-all tw-duration-300 tw-ease-linear group-hover:tw-scale-125"
       />
-      <div class="tw-px-8 tw-w-full tw-pt-6 tw-pb-8 tw-h-full">
-        <h3
-          class="tw-text-uppercase tw-text-xl tw-font-medium tw-pb-4 3xl:tw-text-2xl"
-        >
-          {{ auction.nft.meta.name }}
-        </h3>
-        <div
-          class="tw-w-full tw-flex tw-flex-row tw-items-center tw-justify-between"
-        >
-          <div v-if="!auctionStarted">
-            <div>Starts In</div>
+    </div>
+    <div class="tw-px-8 tw-w-full tw-pt-6 tw-pb-8 tw-h-full">
+      <h3
+        class="tw-text-uppercase tw-text-xl tw-font-medium tw-pb-4 3xl:tw-text-2xl"
+      >
+        {{ auction.nft.meta.name }}
+      </h3>
+      <div
+        class="tw-w-full tw-flex tw-flex-row tw-items-center tw-justify-between"
+      >
+        <div v-if="!auctionStarted">
+          <div>Starts In</div>
+          <reusable-count-down
+            :startTime="auction.startAt"
+            :textSmall="true"
+            @countdownComplete="startAuction"
+          />
+        </div>
+        <div v-if="auctionStarted">
+          <div v-if="!auctionEnded">
+            <div>Ends In</div>
             <reusable-count-down
-              :startTime="auction.startAt"
+              :startTime="auction.endAt"
               :textSmall="true"
-              @countdownComplete="startAuction"
+              @countdownComplete="endAuction"
             />
           </div>
-          <div v-if="auctionStarted">
-            <div v-if="!auctionEnded">
-              <div>Ends In</div>
-              <reusable-count-down
-                :startTime="auction.endAt"
-                :textSmall="true"
-                @countdownComplete="endAuction"
-              />
-            </div>
 
-            <div class="tw-text-wapal-pink" v-else>Ended</div>
-          </div>
-          <div>
-            <div>Current Bid</div>
-            <div class="tw-text-wapal-pink">
-              {{
-                auction.biddings[0] ? auction.biddings[0].bid : auction.min_bid
-              }}
-              APT
-            </div>
+          <div class="tw-text-wapal-pink" v-else>Ended</div>
+        </div>
+        <div>
+          <div>Current Bid</div>
+          <div class="tw-text-wapal-pink">
+            {{
+              auction.biddings[0] ? auction.biddings[0].bid : auction.min_bid
+            }}
+            APT
           </div>
         </div>
-        <ReusableThemeButton
-          title="Place You bid"
-          @click="$router.push('/auctions/' + auction.auction_name)"
-          class="tw-w-full tw-my-5 !tw-py-3"
-        />
       </div>
+      <ReusableThemeButton
+        title="Place You bid"
+        class="tw-w-full tw-my-5 !tw-py-3"
+      />
     </div>
-  </div>
+  </NuxtLink>
 </template>
 <script>
 export default {
