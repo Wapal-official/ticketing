92a407c2fd90049fbd7ffa54f429a3eb9d8e98ea
Meghant00 <dangolmeghant0@gmail.com>
2023-06-10 18:57:53 +0545

    chore: feature netzcuty

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
     <section class="tw-py-4 2xl:tw-container tw-mx-auto">
-      <auction-featured />
+      <featured />
     </section>
     <div>
       <landing-slider :collections="collections" :loading="loading" />
