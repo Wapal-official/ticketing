f50557638a36c068bfcb3198c973810bf684b703
Meghant00 <dangolmeghant0@gmail.com>
2023-06-15 09:49:25 +0545

    Revert "chore: get discord secret from api"
    This reverts commit 753f8fcb0fc60e241c8637a859c0a2283303adef.

--- pages/index.vue
+++ pages/index.vue
@@ -69,7 +69,6 @@ import {
   getLiveCollections,
   getUpcomingCollections,
 } from "@/services/CollectionService";
-import { getDiscordSecret } from "~/services/EnvService";
 
 export default {
   name: "IndexPage",
@@ -153,10 +152,6 @@ export default {
     },
   },
   async created() {
-    const res = await getDiscordSecret();
-
-    console.log(res);
-
     await this.getCollections();
 
     this.auctions = await getUpcomingAuctions({ page: 1, perPage: 4 });
--- services/EnvInterceptor.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const envInterceptor = axios.create({
-  baseURL: "https://staging.wapal.io",
-});
-
-export default envInterceptor;
