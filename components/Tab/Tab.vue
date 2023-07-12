13d1f623988f3fbea09402e1e2fdc75022cd1834
Meghant00 <dangolmeghant0@gmail.com>
2023-07-12 20:50:37 +0545

    chore: change style of bordered tab

--- components/Tab/Tab.vue
+++ components/Tab/Tab.vue
@@ -9,7 +9,7 @@
       v-for="(tab, index) in tabs"
       :key="tab"
       :ripple="false"
-      class="!tw-text-white tw-rounded-full tw-py-2 tw-px-6 !tw-capitalize !tw-text-base !tw-font-medium tw-transition-all tw-duration-200 tw-ease-linear tw-border-solid tw-border tw-border-dark-4 3xl:tw-text-lg hover:tw-border-white hover:!tw-bg-white hover:!tw-text-dark-9"
+      class="!tw-text-white tw-rounded-full !tw-h-[40px] !tw-max-h-[40px] tw-py-2 tw-px-6 !tw-capitalize !tw-text-base !tw-font-medium tw-transition-all tw-duration-200 tw-ease-linear tw-border-solid tw-border tw-border-dark-4 3xl:tw-text-lg hover:tw-border-white hover:!tw-bg-white hover:!tw-text-dark-9 hover:!tw-font-semibold"
       :class="{ 'tw-ml-4': index !== 0 }"
     >
       {{ tab }}
