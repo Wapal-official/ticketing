8bda7d8cbf4afd11a65ef75531c5fff974ee7607
Meghant00 <dangolmeghant0@gmail.com>
2023-10-03 10:12:59 +0545

    chore: use iframe instead of video

--- pages/dashboard/tutorial.vue
+++ pages/dashboard/tutorial.vue
@@ -8,8 +8,8 @@
         v-for="(video, index) in videos"
         :key="index"
       >
-        <div class="tw-rounded-[0.9375rem]">
-          <video
+        <div class="tw-rounded-[0.9375rem] video-container">
+          <!-- <video
             class="tw-rounded-[0.9375rem]"
             controls
             controlslist="fullscreen"
@@ -17,7 +17,18 @@
             <source
               src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
             />
-          </video>
+          </video> -->
+          <iframe
+            class="tw-rounded-[0.9375rem] !tw-w-full"
+            src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
+            id="video"
+            frameborder="0"
+            allowscriptaccess="always"
+            allow="autoplay"
+            webkitallowfullscreen
+            mozallowfullscreen
+            allowfullscreen
+          ></iframe>
         </div>
         <h3 class="tw-text-lg tw-font-bold tw-text-white">{{ video.title }}</h3>
       </div>
@@ -40,3 +51,22 @@ export default {
   },
 };
 </script>
+<style scoped>
+.video-container {
+  position: relative;
+  padding-bottom: 56.25%;
+  overflow: hidden;
+  width: 100%;
+  height: auto;
+}
+
+.video-container iframe,
+.video-container object,
+.video-container embed {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
