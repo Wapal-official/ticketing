306e755c5bfc010d4fd4ad58672a15cf16b31182
Yogesh Shrestha <70264221+Yogeshshrestha@users.noreply.github.com>
2024-06-12 22:43:22 +0545

    loonies feature video

--- components/Landing/AllCollectionSection.vue
+++ components/Landing/AllCollectionSection.vue
@@ -61,6 +61,30 @@ export default {
       const res = await getCollections(1, 16);
 
       this.collections = res;
+      const looniesCollection = {
+        name: "Loonies",
+        description: "",
+        image:
+          "https://wflakeutdvphlghtnureiqqdvu6xhasm5nfi3cpqioiltaaqai2q.arweave.net/sVYFEpMdXnWY820iREIDrT1zgkzrSo2J8EOQuYAQAjU",
+        twitter: "https://x.com/theloonies_nft",
+        website: "https://theloonies.xyz/",
+        discord: "https://t.co/jYRAyXBgfj",
+        mintDetails: {
+          link: " ",
+        },
+        candyMachine: {
+          public_sale_time: "2024-06-13T14:50:00.000Z",
+          resource_account:
+            "0x39f1338e6b69c3ed2f0caa95876e898dbe4c9b272d721626d577554015d033b8",
+          candy_id:
+            "0x25d440284ca6c13afadb0e83ff1bccacbaa75175551111d8b7cb5d2854e708f0",
+        },
+        status: {
+          sold_out: false,
+        },
+        username: "loonies",
+        isVerified: true,
+      };
 
       const collection = {
         name: "Aptos Monkeys Sanctuary",
@@ -88,6 +112,8 @@ export default {
       };
 
       this.collections.unshift(collection);
+      this.collections.unshift(looniesCollection);
+
       this.collections.pop();
       if (this.collections.length < 16) {
         const draftRes = await getApprovedDrafts(
--- components/Landing/FeaturedSlider.vue
+++ components/Landing/FeaturedSlider.vue
@@ -64,34 +64,33 @@ export default {
     };
   },
   async mounted() {
-    const res = await getFeaturedCollection();
-    this.featuredCollections.push(...res);
-
     const collection = {
-      name: "Aptos Monkeys Sanctuary",
-      description:
-        "Sanctuaries are homes to the fighting Monkeys who stood their ground to protect their lands and fortunes.",
-      image:
-        "https://aggregator-cache.wapal.io/api/cache?l=https://arweave.net/z92BGaOGx-DYVTP_O8W31rAsu3PUO7uAQjzgXIoB2vc/199.png",
-      twitter: "https://twitter.com/AptosMonkeys",
-      website: "https://www.aptosmonkeys.club/",
-      discord: "https://discord.com/invite/sFfe75BHQ3",
+      name: "Loonies",
+      description: "",
+      media2:
+        "https://arweave.net/omyEpuu-pSHoDxW6eh7WlzNTRFpndzaMcD1Nu4UvyA8/0.mp4",
+      twitter: "https://x.com/theloonies_nft",
+      website: "https://theloonies.xyz/",
+      discord: "https://t.co/jYRAyXBgfj",
       mintDetails: {
-        link: "https://monkeys.wapal.io",
+        link: " ",
       },
       candyMachine: {
-        public_sale_time: "2023-10-31T06:23:35.216Z",
+        public_sale_time: "2024-06-13T15:00:00.000Z",
         resource_account:
           "0x39f1338e6b69c3ed2f0caa95876e898dbe4c9b272d721626d577554015d033b8",
         candy_id:
           "0x25d440284ca6c13afadb0e83ff1bccacbaa75175551111d8b7cb5d2854e708f0",
       },
       status: {
-        sold_out: true,
+        sold_out: false,
       },
-      username: "monkkesanturies",
+      username: "loonies",
+      isVerified: true,
     };
-    // this.featuredCollections.push(collection);
+    this.featuredCollections.push(collection);
+    const res = await getFeaturedCollection();
+    this.featuredCollections.push(...res);
     const auctionRes = await getFeaturedAuctions();
 
     this.featuredAuctions.push(...auctionRes);
