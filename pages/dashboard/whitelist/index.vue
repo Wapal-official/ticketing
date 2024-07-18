eb0a0927d38a0347f14d21b8af2fd9a9e67333db
Yogesh Shrestha <70264221+Yogeshshrestha@users.noreply.github.com>
2024-07-18 10:51:41 +0545

    wl-remind-time

--- pages/dashboard/whitelist/index.vue
+++ pages/dashboard/whitelist/index.vue
@@ -60,9 +60,71 @@
       </v-tab-item>
     </v-tabs-items>
     <reusable-loading v-else />
+    <v-dialog
+      v-model="showWhitelistSetup"
+      content-class=" !tw-w-full md:!tw-w-1/2 lg:!tw-w-[30%]"
+    >
+      <div
+        class="!tw-bg-dark-9"
+        style="border: 1px solid #25262b; border-radius: 8px"
+      >
+        <p class="tw-text-center tw-py-3 tw-px-5 !tw-mb-0">
+          Caution: Set whitelist an hour before minting!
+        </p>
+        <v-divider></v-divider>
+        <div class="tw-py-3 tw-px-5">
+          <div class="swiper mySwiper" ref="swiper">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">
+                <p class="tw-text-center tw-text-dark-1">
+                  Step 1: Select Collection
+                </p>
+                <img
+                  class="tw-mx-auto"
+                  src="~/assets/img/dialogs/step1.jpg"
+                  alt="step1"
+                  style="width: 100%; max-width: 440px"
+                />
+              </div>
+              <div class="swiper-slide">
+                <p class="tw-text-center tw-text-dark-1">
+                  Step 2: Confirm Collection
+                </p>
+                <img
+                  class="tw-mx-auto"
+                  src="~/assets/img/dialogs/step2.jpg"
+                  alt="step2"
+                  style="width: 100%; max-width: 440px"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="tw-flex tw-px-5 tw-py-3 tw-mt-3 tw-gap-3 tw-justify-between tw-items-center"
+        >
+          <button-primary
+            :fullWidth="true"
+            title="Don’t show this again"
+            :bordered="true"
+            @click="dontShowAgain"
+          ></button-primary>
+          <button-primary
+            title="Remind me later"
+            :fullWidth="true"
+            @click="remindMeLater"
+          ></button-primary>
+        </div>
+      </div>
+    </v-dialog>
   </div>
 </template>
 <script lang="ts">
+// @ts-ignore
+import Swiper from "swiper/swiper-bundle.min";
+
+import "swiper/swiper-bundle.min.css";
+
 import LaunchpadGuide from "~/components/Dashboard/launchpadGuide.vue";
 import {
   getCollectionInCreatorStudio,
@@ -163,6 +225,7 @@ export default {
       ],
       fetchedCollections: false,
       fetchedWhitelists: false,
+      showWhitelistSetup: false,
     };
   },
   computed: {},
@@ -337,12 +400,49 @@ export default {
     redirectToWhitelistPage(whitelist: any) {
       this.$router.push(`/dashboard/whitelist/${whitelist.username}`);
     },
+    dontShowAgain() {
+      localStorage.setItem("showWhitelistSetup", "false");
+      this.showWhitelistSetup = false;
+    },
+    remindMeLater() {
+      const remindTime = new Date().getTime() + 30 * 60 * 1000;
+      localStorage.setItem("remindWhitelistSetup", remindTime.toString());
+      this.showWhitelistSetup = false;
+    },
+    checkWhitelistPopup() {
+      const showWhitelistSetup = localStorage.getItem("showWhitelistSetup");
+      if (showWhitelistSetup === "false") {
+        this.showWhitelistSetup = false;
+        return;
+      }
+
+      const remindTime = localStorage.getItem("remindWhitelistSetup");
+      if (remindTime) {
+        const currentTime = new Date().getTime();
+        if (currentTime < parseInt(remindTime)) {
+          this.showWhitelistSetup = false;
+          return;
+        }
+      }
+
+      this.showWhitelistSetup = true;
+    },
   },
   async mounted() {
+    this.checkWhitelistPopup();
     this.collections = [];
     this.whitelists = [];
+    const user_id = this.$store.state.userStore.user.user_id;
 
     this.loading = false;
+    new Swiper(this.$refs.swiper, {
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      loop: true,
+      grabCursor: true,
+    });
     await this.mapCollections();
   },
 };
